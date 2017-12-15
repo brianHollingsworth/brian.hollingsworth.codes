77726ba2c3f1ae9422f42690c1fcee6d3486b3e5
Brian Hollingsworth <bhollin42@gmail.com>
2017-12-15 03:17:35 -0600

    Fixed launch issue.

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ Object.assign = require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
-// var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-//   ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-//   mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-//   mongoURLLabel = "";
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+  mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+  mongoURLLabel = "";
 
 // if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
 //   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
